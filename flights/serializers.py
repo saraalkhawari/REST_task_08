a204656127048f4298f6d74a0a8cf27ccdf34ec5
Sara Alkhawari <sara.a.alkhawari@gmail.com>
2019-10-26 10:35:55 +0300

    Rest 8 first commit

--- flights/serializers.py
+++ flights/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from datetime import date
 
 from .models import Flight, Booking, Profile
 
@@ -11,15 +12,28 @@ class FlightSerializer(serializers.ModelSerializer):
 
 
 class BookingSerializer(serializers.ModelSerializer):
+	flight = serializers.SlugRelatedField(
+	read_only = True,
+	slug_field = 'destination'
+		)
 	class Meta:
 		model = Booking
 		fields = ['flight', 'date', 'id']
 
 
 class BookingDetailsSerializer(serializers.ModelSerializer):
+	total = serializers.SerializerMethodField()
+	flight = FlightSerializer()
 	class Meta:
 		model = Booking
-		fields = ['flight', 'date', 'passengers', 'id']
+		fields = ['flight', 'date', 'passengers', 'id' , 'total']
+
+	def get_total(self,obj):
+		price = obj.flight.price
+		passengers = obj.passengers
+		total = price*passengers
+		return total 
+
 
 
 class AdminUpdateBookingSerializer(serializers.ModelSerializer):
@@ -50,9 +64,42 @@ class RegisterSerializer(serializers.ModelSerializer):
         new_user.save()
         return validated_data
 
+class UserSerlializer(serializers.ModelSerializer):
+	class Meta :
+		model = User
+		fields = ['first_name','last_name']
+
+
 
 class ProfileSerializer(serializers.ModelSerializer):
+	user = UserSerlializer()
+	past_bookings= serializers.SerializerMethodField()
+	tier= serializers.SerializerMethodField()
 	class Meta:
 		model = Profile
-		fields = ['user', 'miles']
+		fields = ['user', 'miles','past_bookings', 'tier']
+
+	def get_past_bookings(self,obj):
+		bookings= Booking.objects.filter(user = obj.user, date__lt=date.today())
+		serializer = BookingSerializer(bookings, many= True)
+		return serializer.data
+
+	def get_tier (self,obj):
+		miles = obj.miles
+		if miles < 10000 :
+			return 'Blue'
+		if miles < 60000 :
+			return 'Silver'
+		if miles < 100000 :
+			return 'Gold'
+		return 'Platinum' 
+
+
+
+
+
+
+
+
+
 
